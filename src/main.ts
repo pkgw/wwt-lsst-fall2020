00a6562a4e3b08417045e85a8d3c78878fc0d31a
Peter Williams <peter@newton.cx>
2020-10-09 14:44:35 -0400

    Initial import with minor tweaks from @wwtelescope/embed

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import VTooltip from "v-tooltip";
+import Vuex from "vuex";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAdjust, faCompress, faExpand, faMountain, faSearchMinus, faSearchPlus, faSlidersH } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { createPlugin } from "@wwtelescope/engine-vuex";
+import { EmbedSettings } from "@wwtelescope/embed-common";
+
+import Embed from "./Embed.vue";
+
+Vue.config.productionTip = false;
+
+Vue.use(VTooltip);
+Vue.use(Vuex);
+
+const store = new Vuex.Store({});
+
+Vue.use(createPlugin(), {
+  store,
+  namespace: "wwt-embed"
+});
+
+library.add(faAdjust);
+library.add(faCompress);
+library.add(faExpand);
+library.add(faMountain);
+library.add(faSearchMinus);
+library.add(faSearchPlus);
+library.add(faSlidersH);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+const queryParams = new URLSearchParams(window.location.search);
+const settings = EmbedSettings.fromQueryParams(queryParams.entries());
+
+new Vue({
+  store,
+  el: "#app",
+  render: createElement => {
+    return createElement(Embed, {
+      props: {
+        "wwtNamespace": "wwt-embed",
+        "embedSettings": settings
+      }
+    });
+  }
+});
